d66f3f34aacae4f57bf023087a2f570ebd612eb5
Hrant <hrant.sardar@gmail.com>
2020-08-31 12:45:05 +0400

    merge

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+// import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./app/screens/HomeScreen";
-import SignInScreen from "./app/components/SignInScreen";
-import SignUpScreen from "./app/components/SignUpScreen";
+
 import { BasketScreen } from "./app/screens/Basket/BasketScreen";
 import { BottomNavigation } from "react-native-paper";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -13,19 +13,16 @@ import SettingsScreen from "./app/components/SettingsScreen";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-import { YellowBox } from 'react-native';
+import { YellowBox } from "react-native";
 
-
-    YellowBox.ignoreWarnings(['Setting a timer']);
-
+YellowBox.ignoreWarnings(["Setting a timer"]);
 
 function MyTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Basket" component={BasketScreen} />
-      <Tab.Screen name="Settings" component={BasketScreen} />
-      <Tab.Screen name="Profile" component={BasketScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
 }
@@ -35,32 +32,24 @@ export default function App() {
   const [routes] = React.useState([
     { key: "home", title: "Home", icon: "home" },
     { key: "basket", title: "Basket", icon: "cart" },
-    { key: "settins", title: "Settins", icon: "cog" },
+    { key: "settings", title: "Settings", icon: "settings" },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: HomeScreen,
-    albums: BasketScreen,
+    basket: BasketScreen,
     settings: SettingsScreen,
   });
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
+    <NavigationContainer>
+      <BottomNavigation
+        barStyle={{ backgroundColor: "#ff671b" }}
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
+    </NavigationContainer>
 
-    //     <Stack.Screen
-    //       name="Home"
-    //       component={HomeScreen}
-    //       options={{ title: "Welcome" }}
-    //     /><Stack.Screen name="Basket" component={BasketScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-
-    <BottomNavigation
-    barStyle={{ backgroundColor: "#ff671b" }}
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-    />
     // <NavigationContainer>
     //   <Stack.Navigator>
     //     <Stack.Screen name="Settings" component={SettingsScreen} />
